8f43fde3f4d932b1ba5b47a2424eedac963b7428
Максим Капустин <90614964+kapustin-mi@users.noreply.github.com>
2021-09-16 00:45:52 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+package cs.vsu.ru.kapustin;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double t1 = readCharacteristics();
+        double t2 = readCharacteristics();
+        double v1 = readCharacteristics();
+        double v2 = readCharacteristics();
+
+        double v3 = findTheVolume(v1, v2);
+        double t3 = findTemperature(t1, t2, v1, v2);
+
+        printMixCharacteristics(v3, t3);
+    }
+    private static double readCharacteristics() {
+        Scanner scanner = new Scanner(System.in);
+
+        double num;
+        if (scanner.hasNextDouble()) {
+            num = scanner.nextDouble();
+        } else {
+            System.out.print("Invalid value! Try again.");
+            return readCharacteristics();
+        }
+        return num;
+    }
+    private static double findTheVolume(double v1, double v2) {
+        double v3 = v1+v2;
+        return v3;
+    }
+    private static double findTemperature(double t1, double t2, double v1, double v2) {
+        double t3 = (t1*v1+t2*v2)/(v1+v2);
+        return t3;
+    }
+    private static void printMixCharacteristics(double v3, double t3) {
+        System.out.println("Mixture temperature is: "+t3);
+        System.out.print("The volume of the mixture is: "+v3);
+    }
+}
+
