f65fb5a70c2ddf2d47e03cc78b28900160c8d2bb
Максим Капустин <90614964+kapustin-mi@users.noreply.github.com>
2021-09-18 00:31:27 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -26,7 +26,6 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         double num;
-
             while (!scanner.hasNextDouble()) {
                 System.out.print("Invalid value! Please, try again: ");
                 scanner.next();
