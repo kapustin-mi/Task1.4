2dec61140499f3ac7602ba369881efc8347c2577
Максим Капустин <90614964+kapustin-mi@users.noreply.github.com>
2021-09-29 22:12:34 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class Main {
         double v1 = readCharacteristics("first volume");
         double v2 = readCharacteristics("second volume");
 
-        double v3 = findTheVolume(v1, v2);
+        double v3 = findVolume(v1, v2);
         double t3 = findTemperature(t1, t2, v1, v2);
 
         printMixCharacteristics(v3, t3);
@@ -34,14 +34,12 @@ public class Main {
         return num;
     }
 
-    private static double findTheVolume(double v1, double v2) {
-        double v3 = v1 + v2;
-        return v3;
+    private static double findVolume(double v1, double v2) {
+        return v1 + v2;
     }
 
     private static double findTemperature(double t1, double t2, double v1, double v2) {
-        double t3 = (t1 * v1 + t2 * v2) / (v1 + v2);
-        return t3;
+        return (t1 * v1 + t2 * v2) / (v1 + v2);
     }
 
     private static void printMixCharacteristics(double v3, double t3) {
